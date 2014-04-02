51ce97ccb97f4210973dcf3a7b8b12dcc446f8df
Charles Hoffman <chuck.hoffman@banno.com>
2014-04-02 10:30:19 -0500

    url elements with loc elements in them

--- src/main/scala/com/banno/sitemapgenerator4s/SitemapGenerator.scala
+++ src/main/scala/com/banno/sitemapgenerator4s/SitemapGenerator.scala
@@ -2,8 +2,15 @@ package com.banno.sitemapgenerator4s
 
 class SitemapGenerator(baseUrl: String) {
 
-  def xml =
+  var urls: Seq[String] = Seq()
+
+  def xml = {
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      { urls.map(u => <url><loc>{u}</loc></url>) }
     </urlset>
+  }
+
+  def addUrl(url: String) { urls +:= url }
+
 
 }
--- src/test/scala/com/banno/sitemapgenerator4s/SitemapGeneratorSpec.scala
+++ src/test/scala/com/banno/sitemapgenerator4s/SitemapGeneratorSpec.scala
@@ -6,13 +6,38 @@ import scala.xml._
 
 class SitemapGeneratorSpec extends Specification {
   "the sitemap generator" should {
+
     "generate xml containing an urlset element" in new context {
       generator.xml.label mustEqual "urlset"
       generator.xml.namespace mustEqual(
         "http://www.sitemaps.org/schemas/sitemap/0.9")
     }
 
+    "start out with no url elements (no pages)" in new context {
+      generator.xml \\ "url" must be empty
+    }
 
+    "Have one url element for each url added" in new context {
+      generator.addUrl("http://www.example.com/")
+      (generator.xml \\ "url" length) mustEqual(1)
+      generator.addUrl("http://www.example.com/blog")
+      (generator.xml \\ "url" length) mustEqual(2)
+    }
+
+    "place each url in a loc element within its url element" in new context {
+      generator.addUrl("http://www.example.com/blog")
+      val urlElement = (generator.xml \ "url").head
+      val locElements = (urlElement \ "loc")
+      locElements.length mustEqual(1)
+      locElements.head.text mustEqual("http://www.example.com/blog")
+    }
+
+    // take paths as well as full urls
+    // error on invalid urls
+    // take URL objects as well as strings
+    // take other url info (lastmod, changefreq, priority)
+    // validate the above, error
+    // check when list of urls gets too big (max 50k)
   }
 }
 
